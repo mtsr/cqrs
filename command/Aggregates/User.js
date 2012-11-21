84b89a32bf89746980e0a8f0ba5b8d0050a1778b
jmatser <jonas@jonasmatser.nl>
2012-11-21 09:29:43 +0100

    Added businessRules to User

--- command/Aggregates/User.js
+++ command/Aggregates/User.js
@@ -4,16 +4,28 @@ var User = Aggregate.extend({
     // Commands
 
     registerUser: function(data, callback) {
+        console.log('User.registerUser');
         this.apply(this.toEvent('userRegistered', data));
 
-        // TODO check business rules
+        this.checkBusinessRules(callback);
     },
 
     // Events
 
     userRegistered: function(data) {
+        console.log('User.userRegistered');
         this.set(data);
-    }
+    },
+
+    // Business rules
+    // TODO move to separate file?
+    // TODO fix checkBusinessRules to work without business rules as well
+
+    businessRules: [
+        function(changed, previous, events, callback) {
+            callback(null);
+        }
+    ],
 });
 
 module.exports = User;
