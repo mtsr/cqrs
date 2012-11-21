cc4f3dccb0ae174e2e60f326adfefc29b3721bff
jmatser <jonas@jonasmatser.nl>
2012-11-21 09:30:29 +0100

    Fixed typo uncomittedEvents Added checkBusinessRules

--- command/Bases/Aggregate.js
+++ command/Bases/Aggregate.js
@@ -4,7 +4,7 @@ var Base = require('./Base');
 var Aggregate = Base.extend({
     constructor: function(id) {
         this.id = id;
-        this.uncommitedEvents = [];
+        this.uncommittedEvents = [];
         this.attributes = { id: id, revision: 0 };
     },
 
@@ -40,6 +40,7 @@ var Aggregate = Base.extend({
     },
 
     loadFromHistory: function(data, events) {
+        console.log('Aggregate.loadFromHistory');
         if (data) {
             this.set(data);
         }
@@ -53,6 +54,7 @@ var Aggregate = Base.extend({
     },
 
     apply: function(events, callback) {
+        console.log('Aggregate.apply');
         var self = this;
 
         if (!_.isArray(events)) {
@@ -70,6 +72,7 @@ var Aggregate = Base.extend({
         });
 
         _.each(historyEvents, function(evt) {
+            console.log(evt);
             self[evt.event](evt.payload);
         });
 
@@ -86,34 +89,34 @@ var Aggregate = Base.extend({
         return;
     },
 
-    // checkBusinessRules: function(callback) {
-    //     var self = this;
-    //     var changedAttributes = this.toJSON();
-    //     var keys = [];
+    checkBusinessRules: function(callback) {
+        var self = this;
+        var changedAttributes = this.toJSON();
+        var keys = [];
 
-    //     if(!this.businessRules) return callback(null);
+        if(!this.businessRules) return callback(null);
 
-    //     this.businessRules.forEach(function(rule, index) {
-    //         rule.call(self, changedAttributes, self.previousAttributes, self.uncommittedEvents, function(ruleId, message) {
-    //             if (ruleId) {
-    //                 if (!message) {
-    //                     message = ruleId;
-    //                     ruleId = arguments.callee.caller.name;
-    //                 }
-    //                 keys.push({ type: 'businessRule', ruleId: ruleId, message: message });
-    //             }
-    //         });
+        this.businessRules.forEach(function(rule, index) {
+            rule.call(self, changedAttributes, self.previousAttributes, self.uncommittedEvents, function(ruleId, message) {
+                if (ruleId) {
+                    if (!message) {
+                        message = ruleId;
+                        ruleId = arguments.callee.caller.name;
+                    }
+                    keys.push({ type: 'businessRule', ruleId: ruleId, message: message });
+                }
+            });
 
-    //         if (index === self.businessRules.length - 1) {
-    //             if (keys.length > 0) {
-    //                 self.attributes = self.previousAttributes;
-    //                 callback(keys);
-    //             } else {
-    //                 callback(null);
-    //             }
-    //         }
-    //     });
-    // }
+            if (index === self.businessRules.length - 1) {
+                if (keys.length > 0) {
+                    self.attributes = self.previousAttributes;
+                    callback(keys);
+                } else {
+                    callback(null);
+                }
+            }
+        });
+    }
 });
 
 module.exports = Aggregate;
