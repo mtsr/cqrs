c042ba3f723ae859b2ef2e79bda10f3a6589c40f
jmatser <jonas@jonasmatser.nl>
2012-11-27 09:26:10 +0100

    Keep User collection for Chat projection TODO: Update user documents by merging instead of overwriting

--- projection/Projections/Chat.js
+++ projection/Projections/Chat.js
@@ -1,4 +1,5 @@
 var _ = require('lodash');
+var async = require('async');
 
 var Projection = require('../Bases/Projection');
 
@@ -10,29 +11,81 @@ var Chat = Projection.extend({
         this.events = {
             Chat: [
                 'chatStarted',
-                'messageSent'
+                // 'messageSent'
+            ],
+            User: [
+                'userRegistered',
+                'nameChanged'
             ]
         };
 
         this.collections = {
-            Chat: null
+            Chat: null,
+            User: null
         };
     },
 
     chatStarted: function(event) {
+        var self = this;
+
         console.log('Chat started', event);
+
         var doc = { aggregateID: event.aggregateID };
         _.extend(doc, event.data);
-        this.collections.Chat.insert(doc, { safe: true }, function(err, result) {
+
+        async.map(doc.users, function(user, next) {
+            self.collections.User.findOne({ aggregateID: user }, function(err, result) {
+                if (err) {
+                    return next(err);
+                }
+                next(null, result);
+            });
+        }, function(err, results) {
+            if (err) {
+                return console.log(err);
+            };
+            doc.users = results;
+            self.collections.Chat.insert(doc, { safe: true }, function(err, result) {
+                if (err) {
+                    console.log(err);
+                    throw err;
+                }
+            });
+        });
+    },
+
+    messageSent: function(event) {
+        console.log('Message sent', event);
+        this.collections.Chat.update({ aggregateID: event.aggregateID }, { $push: { messages: event.data }}, { safe: true }, function(err, result) {
             if (err) {
                 console.log(err);
                 throw err;
             }
         });
     },
-    messageSent: function(event) {
-        console.log('Message sent', event);
-        this.collections.Chat.update({ aggregateID: event.aggregateID }, { $push: { messages: event.data }}, { safe: true }, function(err, result) {
+
+    userRegistered: function(event) {
+        console.log('User registered', event);
+        var doc = { aggregateID: event.aggregateID };
+        _.extend(doc, event.data);
+        this.collections.User.insert(doc, { safe: true }, function(err, result) {
+            if (err) {
+                console.log(err);
+                throw err;
+            }
+        });
+    },
+
+    nameChanged: function(event) {
+        console.log('Name changed', event);
+        this.collections.User.update({ aggregateID: event.aggregateID }, { $set: event.data }, { safe: true }, function(err, result) {
+            if (err) {
+                console.log(err);
+                throw err;
+            }
+        });
+
+        this.collections.Chat.update({ "users.aggregateID": event.aggregateID }, { $set: { "users.$": event.data } }, function(err, result) {
             if (err) {
                 console.log(err);
                 throw err;
