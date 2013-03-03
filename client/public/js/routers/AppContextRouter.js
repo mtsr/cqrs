bf0d481b8f96762050a5e4a11a33a5f9c6fe3837
Jonas Matser <jonas@jonasmatser.nl>
2013-03-03 16:04:55 +0100

    Added comment

--- client/public/js/routers/AppContextRouter.js
+++ client/public/js/routers/AppContextRouter.js
@@ -7,6 +7,8 @@ define([
 ], function ( $, _, Backbone, Marionette, Geppetto ) {
   AppContextRouter = Backbone.Marionette.AppRouter.extend({
     initialize: function(options) {
+      // TODO use bindContext here if for whatever reason we need to listen to events to
+      // runs into an initialization order problem, though
       this.context = options.context;
     },
 
