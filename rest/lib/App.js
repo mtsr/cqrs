f5cadfb61b7f21c7f822768469a01a927d152e2d
Jonas Matser <jonas@jonasmatser.nl>
2013-03-28 09:08:18 +0100

    Cleanup

--- rest/lib/App.js
+++ rest/lib/App.js
@@ -14,20 +14,7 @@ var App = function() {
 };
 
 App.prototype.start = function(ready) {
-  var app = express();
-
-  app.configure(function() {
-    app.set('port', process.env.PORT || 3001);
-    app.set('views', __dirname + '/views');
-    app.set('view engine', 'jade');
-    app.use(express.favicon());
-    app.use(express.logger('dev'));
-    app.use(express.bodyParser());
-    app.use(express.methodOverride());
-    app.use(express.cookieParser('your secret here'));
-    app.use(express.session());
-    app.use(app.router);
-  });
+  var app = setupExpress();
 
   async.auto({
     'commandHandler': [function(done, results) {
@@ -135,6 +122,25 @@ App.prototype.start = function(ready) {
   }, function(err, results) {
     ready && typeof ready === 'function' && ready();
   });
+};
+
+var setupExpress = function() {
+  var app = express();
+
+  app.configure(function() {
+    app.set('port', process.env.PORT || 3001);
+    app.set('views', __dirname + '/views');
+    app.set('view engine', 'jade');
+    app.use(express.favicon());
+    app.use(express.logger('dev'));
+    app.use(express.bodyParser());
+    app.use(express.methodOverride());
+    app.use(express.cookieParser('your secret here'));
+    app.use(express.session());
+    app.use(app.router);
+  });
+
+  return app;
 };
 
 module.exports = App;
